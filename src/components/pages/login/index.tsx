5cd043b5d83c5c7d7137a0ba80910eabd2b6ed8e
PlayRunOffc <playruns3@gmail.com>
2026-05-19 22:20:44 -0300

    sistema de login com google

--- src/components/pages/login/index.tsx
+++ src/components/pages/login/index.tsx
@@ -1,9 +1,21 @@
-export const LoginPage = () => {
+import { authClient } from "@/lib/auth-client";
+
+export function LoginPage() {
+    async function handleLoginWithGoogle() {
+        await authClient.signIn.social({
+            provider: "google",
+            callbackURL: import.meta.env.URL,
+        });
+    }
 
     return (
-        <div>
-            Login
-        </div>
-    )
-
+        <main className="min-h-screen flex items-center justify-center">
+            <button
+                onClick={handleLoginWithGoogle}
+                className="rounded-xl border px-4 py-2 duration-200 hover:bg-gray-100 active:bg-gray-200/10 cursor-pointer"
+            >
+                Entrar com Google
+            </button>
+        </main>
+    );
 }
